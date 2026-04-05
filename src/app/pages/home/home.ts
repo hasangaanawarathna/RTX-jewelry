287cac7010dc3cacbf40d17300e5b056f785c045
Hasanga-Anawarathna <hasangaanawarathna@gmail.com>
2026-04-05 10:57:17 +0530

    Refactor app component and home page structure

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,2 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
-})
-export class Home {}
+export class Home {
+}
