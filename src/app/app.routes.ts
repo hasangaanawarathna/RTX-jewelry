cc36018708fc77477f63a1663b11bca5c8c546e3
Hasanga-Anawarathna <hasangaanawarathna@gmail.com>
2026-04-06 11:29:18 +0530

    Remove unused contact page and related test files; clean up admin dashboard and login tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Products } from './pages/products/products';
 import { ProductDetails } from './pages/product-details/product-details';
-import { Contact } from './pages/contact/contact';
 import { Feedback } from './pages/feedback/feedback';
 import { Offers } from './pages/offers/offers';
 import { DesignRequest } from './pages/design-request/design-request';
@@ -13,7 +12,6 @@ export const routes: Routes = [
   { path: '', component: Home },
   { path: 'products', component: Products },
   { path: 'products/:id', component: ProductDetails },
-  { path: 'contact', component: Contact },
   { path: 'feedback', component: Feedback },
   { path: 'offers', component: Offers },
   { path: 'design-request', component: DesignRequest },
